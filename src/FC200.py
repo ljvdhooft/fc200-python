a7fc3791f1a33f98250daeb04fe88bf761a51027
ljvdhooft <ljvdhooft@gmail.com>
2026-01-25 17:30:51 +0100

    feat: added FAVORITE_PARAMETERS list

--- src/FC200.py
+++ src/FC200.py
@@ -23,6 +23,7 @@ MIN_PAGE = 0
 MAX_PAGE = 2
 LOOP_MAPPING = [0, 1, 2, 3, 4, 6, 7, 8, 9]
 LOOP_VOLUME = 5 
+FAVORITE_PARAMETERS = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
 class FC200(ControlSurface):
     def __init__(self, c_instance):
