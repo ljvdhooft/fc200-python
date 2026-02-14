8ee9c2a614e3322ab3c2993f3d85ea28223e3ea7
ljvdhooft <ljvdhooft@gmail.com>
2026-02-14 15:16:08 +0100

    fix: Changed target parameters

--- src/config.py
+++ src/config.py
@@ -1,10 +1,10 @@
 TRACK = 0
-MAIN_DEVICE = 1
+MAIN_DEVICE = 3
 MIN_PAGE = 0
 MAX_PAGE = 2
 LOOP_MAPPING = [0, 1, 2, 3, 4, 6, 7, 8, 9]
 LOOP_VOLUME = 5 
-FAVORITE_PARAMETERS = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+FAVORITE_PARAMETERS = [6, 1, 1, 1, 1, 1, 2, 1, 2, 1]
 HIGHLIGHT_NOTES = {
         "tuner": 0,
         "board": 1,
