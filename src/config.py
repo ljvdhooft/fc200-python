be65cc592aa5f388d4ffd04ecdf1c65e3f1aef99
ljvdhooft <ljvdhooft@gmail.com>
2026-02-26 18:34:07 +0100

    feat: created Settings class and listener function for Ableton Link

--- src/config.py
+++ src/config.py
@@ -1,17 +1,18 @@
-PRESET_FOLDER = "/Users/ljvdhooft/Music/Ableton/User Library/eGit presets/"
-TRACK = 0
-MAIN_DEVICE = 3
-MIN_PAGE = 0
-MAX_PAGE = 2
-LOOP_MAPPING = [0, 1, 2, 3, 4, 6, 7, 8, 9]
-LOOP_VOLUME = 5 
-FAVORITE_PARAMETERS = [6, 1, 1, 1, 1, 1, 2, 1, 2, 1]
-HIGHLIGHT_NOTES = {
-        "tuner": 0,
-        "board": 1,
-        "0": 2
-        }
-FORSCORE_PREV_PAGE = 0
-FORSCORE_NEXT_PAGE = 1
-FORSCORE_PREV_SCORE = 2
-FORSCORE_NEXT_SCORE = 3
+class Settings:
+    PRESET_FOLDER = "/Users/ljvdhooft/Music/Ableton/User Library/eGit presets/"
+    TRACK = 32
+    MAIN_DEVICE = 4
+    MIN_PAGE = 0
+    MAX_PAGE = 2
+    LOOP_MAPPING = [0, 1, 2, 3, 4, 6, 7, 8, 9]
+    LOOP_VOLUME = 5 
+    FAVORITE_PARAMETERS = [6, 1, 1, 1, 1, 1, 2, 1, 2, 1]
+    HIGHLIGHT_NOTES = {
+            "tuner": 0,
+            "board": 1,
+            "0": 2
+            }
+    FORSCORE_PREV_PAGE = 0
+    FORSCORE_NEXT_PAGE = 1
+    FORSCORE_PREV_SCORE = 2
+    FORSCORE_NEXT_SCORE = 3
